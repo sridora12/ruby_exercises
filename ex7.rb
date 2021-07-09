37400cca6f889ea2c743fe68f5d9232233cde009
sridora12 <87181979+sridora12@users.noreply.github.com>
2021-07-09 03:41:51 -0700

    Add files via upload

---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Marry had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Marry went."
+puts "." * 10 # What'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "S"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Watch that print Vs . puts on this line What's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
